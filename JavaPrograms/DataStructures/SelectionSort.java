ab1b1e60a38825d33949a8b2250f056fe509eb67
TSSumanth <44135288+TSSumanth@users.noreply.github.com>
2020-04-12 22:26:56 +0530

    Create SelectionSort.java

---
+++ JavaPrograms/DataStructures/SelectionSort.java
@@ -0,0 +1,58 @@
+package Sorting;
+
+public class SelectionSort {
+
+	public static void main(String[] args) 
+	{
+		int[] unsortedArray = {10,29,-20,1,1,1234,787,324,1,35 };
+		SelectionSortSmallestToLeft(unsortedArray);
+		
+	}
+	
+	public static void SelectionSortLargestToRight(int[] givenArray)
+	{
+		int indexOfSortedArray = givenArray.length - 1;
+		while(indexOfSortedArray > 0) {
+			int largestElementIndex = 0;
+			int largestElement = givenArray[0];
+			for(int i=1;i<=indexOfSortedArray;i++)
+			{
+				if(givenArray[i] > largestElement) {
+					largestElementIndex = i;
+					largestElement = givenArray[i];
+				}
+			}
+			int temp = givenArray[largestElementIndex];		
+			givenArray[largestElementIndex] = givenArray[indexOfSortedArray];
+			givenArray[indexOfSortedArray] = temp;
+			indexOfSortedArray--; 
+		}
+		
+		for(int i: givenArray)
+			System.out.println(i);
+	}
+
+	
+	public static void SelectionSortSmallestToLeft(int[] givenArray)
+	{
+		int indexOfSortedArray = 0;
+		while(indexOfSortedArray < givenArray.length -1) {
+			int smallestElementIndex = givenArray.length -1;
+			int smallestElement = givenArray[givenArray.length -1];
+			for(int i=givenArray.length -2;i<=indexOfSortedArray;i++)
+			{
+				if(givenArray[i] < smallestElement) {
+					smallestElementIndex = i;
+					smallestElement = givenArray[i];
+				}
+			}
+			int temp = givenArray[smallestElementIndex];		
+			givenArray[smallestElementIndex] = givenArray[indexOfSortedArray];
+			givenArray[indexOfSortedArray] = temp;
+			indexOfSortedArray++; 
+		}
+		
+		for(int i: givenArray)
+			System.out.println(i);
+	}
+}
