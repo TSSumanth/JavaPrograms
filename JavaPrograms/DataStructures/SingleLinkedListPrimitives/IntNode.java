9f4113431ee0a148c9b55f1f0059b55a0d0d9691
TSSumanth <44135288+TSSumanth@users.noreply.github.com>
2020-04-14 16:27:33 +0530

    Create IntNode.java

---
+++ JavaPrograms/DataStructures/SingleLinkedListPrimitives/IntNode.java
@@ -0,0 +1,20 @@
+package SingleLinkedListPrimitives;
+
+public class IntNode 
+{
+	private int data;
+	IntNode nextReference;
+	
+	IntNode(int data)
+	{
+		this.data = data;
+	}
+	
+	
+	public String toString()
+	{
+		return this.data+"";
+	}
+
+
+}
