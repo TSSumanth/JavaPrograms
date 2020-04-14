6d5639fc365d39a921c9f2c8701e08f0af1c2d81
TSSumanth <44135288+TSSumanth@users.noreply.github.com>
2020-04-14 16:29:59 +0530

    Create EmployeeNode.java

---
+++ JavaPrograms/DataStructures/SingleLinkedLIstCustomClasses/EmployeeNode.java
@@ -0,0 +1,15 @@
+package SingleLinkedLIstCustomClasses;
+
+public class EmployeeNode {
+	Employee data;
+	EmployeeNode nextNodeReference;
+	
+	EmployeeNode(Employee data,EmployeeNode nextNodeReference)
+	{
+		this.data = data;
+		this.nextNodeReference = nextNodeReference;
+	}
+	
+	
+
+}
